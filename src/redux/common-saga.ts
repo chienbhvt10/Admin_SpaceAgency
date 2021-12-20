8f74f29a074ffc3014dce5f454a554a9dba6bab7
sybv <sybv@its-global.vn>
2021-12-20 18:32:31 +0700

    update

--- src/redux/common-saga.ts
+++ src/redux/common-saga.ts
@@ -1,12 +1,18 @@
 import { put, take } from 'redux-saga/effects';
 import { NotificationError, NotificationSuccess } from 'commons/components/Notification';
 import { actionLoadingError } from './actions';
+import { getNavigate } from 'helpers/history';
+import { CommonPath } from 'commons/base-routes';
 export interface ResponseGenerator {
   payload: any;
 }
 export function* checkErrorAsync() {
   while (true) {
     const action: ResponseGenerator = yield take((action: any) => /.+_ERROR/.test(action.type));
+    if (action.payload.response.errors[0].extensions.code === 'UNAUTHENTICATED') {
+      getNavigate(CommonPath.LOGIN_PATH);
+      return;
+    }
     yield put(actionLoadingError());
     let message = action.payload.message || 'Có lỗi xảy ra vui lòng thử lại sau';
     if (action.payload.response && action.payload.response?.errors?.length) {
