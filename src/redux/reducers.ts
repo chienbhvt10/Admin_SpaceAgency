16636827c02085fa9b9497aeb840643c46d11507
Duong Cong Chien <chienzxzx33@gmail.com>
2022-01-10 11:03:04 +0700

    update stylecollection ui

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import auth, { AuthModuleState } from 'modules/Auth/redux/reducers';
 import user, { UserModuleState } from 'modules/UserManagement/redux/reducers';
-import style, { StyleModuleState } from 'modules/StylesCollection/redux/reducers';
 
 import loadingReducer from '../utils/loading/redux/loadingReducer';
 import { LoadingState } from './action-types';
@@ -9,7 +8,6 @@ import { LoadingState } from './action-types';
 export interface RootState {
   auth: AuthModuleState;
   user: UserModuleState;
-  style: StyleModuleState;
   loadingReducer: LoadingState;
 }
 
@@ -17,5 +15,4 @@ export default combineReducers<RootState>({
   auth,
   loadingReducer,
   user,
-  style,
 });
