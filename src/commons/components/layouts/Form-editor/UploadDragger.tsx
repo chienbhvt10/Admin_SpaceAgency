c323646be4698dfcffdc278c93aea468296fa5e6
tuanna12 <tuanna@its-global.vn>
2022-05-10 12:06:39 +0700

    revert

--- src/commons/components/layouts/Form-editor/UploadDragger.tsx
+++ src/commons/components/layouts/Form-editor/UploadDragger.tsx
@@ -107,7 +107,7 @@ export default function UploadDragger(props: IProps) {
           </Modal>
           <input
             type="image"
-            src={state.imageUrl.startsWith('https')? state.imageUrl:urlImage + state.imageUrl}
+            src={urlImage + state.imageUrl}
             value={props.value?.id}
             className="image"
             alt="avatar"
